de944c803329f363e8fd30a0da2cdfdb10cbd8f6
zhongpei <1000@intry.cn>
2024-08-24 14:30:46 +0800

    pnpm i -D @types/multer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { swaggerConfigure } from './configures/swaggerConfigure';
+import { corsConfigure } from './configures/corsConfigure';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  swaggerConfigure(app, { path: '/' });
+
+  corsConfigure(app);
+
+  // 假设你已经通过 ConfigModule 设置了配置
+  const configService = app.get(ConfigService); // 通过依赖注入获取 ConfigService
+
+  const port = configService.get<number>('HOST_PORT') || 3000; // 从配置中获取 PORT
+
+  const hostname = configService.get<string>('HOST_NAME'); // 从配置中获取 PORT
+
+  await app.listen(port, hostname, () => {
+    Logger.warn(`[hostname] ${hostname}:${port}`);
+  });
 }
 bootstrap();
