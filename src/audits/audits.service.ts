29e1fea5c29489fd38afc14af816f7c73fe9b220
zhongpei <1000@intry.cn>
2024-09-07 18:20:56 +0800

    isErr

--- src/audits/audits.service.ts
+++ src/audits/audits.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import e from 'dbschema/edgeql-js'; // auto-generated code
 import { client } from 'src/edgedb';
 import { Request, Response } from 'express';
@@ -14,6 +14,14 @@ export class AuditsService {
   ) {
     const { method, url, headers } = request;
     Logger.log(`[END] [${method}] ${url} [${duration}ms]`, AuditsService.name);
+    Logger.log(error.statusCode, 'error.statusCode');
+
+    const isErr = !!error.message;
+    const http_status = isErr
+      ? error instanceof HttpException
+        ? error.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR
+      : response.statusCode;
 
     const insert = e.insert(e.audit.AuditLog, {
       app_name: e.str('NestJS'),
@@ -23,14 +31,18 @@ export class AuditsService {
       browser_info: e.str(headers['user-agent']),
       handler_name: e.str(handler_name),
       http_method: e.str(method),
-      http_status: e.int64(response.statusCode),
+      http_status: e.int64(http_status),
       url: e.str(url),
       data: e.json(data || {}),
-      error: e.json({
-        message: error['message'],
-        stack: error.stack,
-        date: Date.now(),
-      }),
+      error: e.json(
+        isErr
+          ? {
+              message: error['message'],
+              stack: error.stack,
+              date: Date.now(),
+            }
+          : {},
+      ),
       headers: e.json(headers || {}),
     });
 
