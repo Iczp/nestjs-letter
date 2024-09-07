c5623dd7bd5193ad652da7d533fccfb6f1d63b9c
zhongpei <1000@intry.cn>
2024-09-07 09:12:16 +0800

    0

--- src/audits/AuditInterceptor.ts
+++ src/audits/AuditInterceptor.ts
@@ -5,16 +5,25 @@ import {
   Logger,
   NestInterceptor,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuditInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const response = context.switchToHttp().getResponse();
-    const { method, url } = request;
+    const { method, url, headers, params, query, body } = request;
     const startTime = Date.now();
+
+    // 记录请求的 headers、params、query、body 信息
+    // console.log(`Request: ${method} ${url}`);
+    // console.log('Headers:', headers);
+    // console.log('Params:', params);
+    // console.log('Query:', query);
+    // console.log('Body:', body);
+
+    const handler = context.getHandler();
 
     // console.log(`Request: ${method} ${url}`);
 
@@ -25,8 +34,27 @@ export class AuditInterceptor implements NestInterceptor {
         console.log(
           `Response: ${method} ${url} - Status: ${response.statusCode} - Duration: ${duration}ms`,
         );
+        // console.log(
+        //   `query: ${JSON.stringify(query)} ${JSON.stringify(params)} - body: ${JSON.stringify(body)} - Duration: ${duration}ms`,
+        // );
+        // console.log(`headers: ${JSON.stringify(headers)} `);
+        Logger.log(`[${handler.name}]`, 'Auditing');
         // 执行自定义审计逻辑，例如将审计记录保存到数据库
         this.logAudit(request, response, duration, data);
+      }),
+      catchError((error) => {
+        const endTime = Date.now();
+        const duration = endTime - startTime;
+
+        console.log(
+          `Error Response: ${method} ${url} - Status: ${response.statusCode || 500} - Duration: ${duration}ms - Error: ${error.message}`,
+        );
+
+        // 记录失败请求的审计信息
+        // this.logAudit(request, response, duration, null, error);
+
+        // 继续抛出异常
+        return throwError(() => error);
       }),
     );
   }
@@ -35,6 +63,9 @@ export class AuditInterceptor implements NestInterceptor {
   logAudit(request: any, response: any, duration: number, data: any) {
     // 这里可以执行实际的审计逻辑，例如保存到数据库或记录到日志文件
     const { method, url } = request;
-    Logger.log(`[${method}] ${url} [${duration}ms]`, 'Auditing');
+    Logger.log(
+      `[END] [${method}] ${url} [${duration}ms]`,
+      AuditInterceptor.name,
+    );
   }
 }
