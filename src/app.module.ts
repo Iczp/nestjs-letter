74b2e9fd4fade9fb381355754617f1116785a92d
zhongpei <1000@intry.cn>
2024-09-09 14:10:49 +0800

    0

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { ActivityCustomerModule } from './activity-customer/activity-customer.module';
+import { ActivityCustomerModule } from './activity-customers/activity-customer.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
