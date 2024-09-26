e3ffea4587b23e4fe893d581b08217d855713a91
zhongpei <1000@intry.cn>
2024-09-26 13:46:51 +0800

    0

--- scripts/update-docker-compose-version.js
+++ scripts/update-docker-compose-version.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
-// const yaml = require('js-yaml');
-const yaml = require('yaml-comment-include');
+const yaml = require('js-yaml');
 const path = require('path');
 const packageJson = require('../package.json');
 const version = packageJson.version;
